fc9f3139eb25542e3de3691b609b2b720e0d1f56
Karolis Narkevicius <karolis.n@gmail.com>
2018-08-16 10:30:31 +0100

    Check if platform is browser before registering with smartserve

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -7,3 +7,11 @@ export function log (...args: any[]) {
     }
   }
 }
+
+export function logError (...args: any[]) {
+  if (typeof window !== 'undefined' && window.console) {
+    if (window.__qubit && window.__qubit.angular && window.__qubit.angular.debug) {
+      console.error(...args)
+    }
+  }
+}
