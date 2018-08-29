16080cc30cd78e79d89da25a76838de720eaf1e4
Karolis Narkevicius <karolis.n@gmail.com>
2018-08-29 15:17:27 +0100

    Create a glue module index+esm to avoid exporting experience fn as default

--- examples/experience-2/activation.ts
+++ examples/experience-2/activation.ts
@@ -1,4 +1,4 @@
-import experience from '../../src/experience'
+import experience from '../../src/experience/esm'
 
 export default function activation (options: any, cb: any) {
   const release = experience(options).register(['article-preview'], (slots: any) => {
