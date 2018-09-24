9fdcbb9608ac7acca6c7a4ea1cbaac1cc3cbe72d
Augstin Grigorov <augustin.grigorov@qubit.com>
2018-09-24 16:10:16 +0100

    Add comments

--- src/experience/index.ts
+++ src/experience/index.ts
@@ -1,2 +1,8 @@
+// This is to appease typescript
+// Or more specifically, this is to avoid having to type:
+// `require('qubit-angular/experience').default`
+// by splitting experience file into esm.js and index.js, we can import
+// it in TypeScript code in demo dir as ES6 import with `import '../../experience/esm'`
+// and import in experiences as CJS require with `require('qubit-angular/experience')`
 import experience from './esm'
 export = experience
